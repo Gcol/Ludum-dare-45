7bb122920ca8417b9e882b9134605fac12f1343f
Tyraen1er <Emmanuel.Ferrandini@yahoo.fr>
2019-10-05 16:16:21 +0200

    norme nom dossier image

---
+++ Assets/Perso.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perso : MonoBehaviour
+{
+
+   	private Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = 0;
+        float y = 0;
+        rigidBody.velocity = new Vector3(0,0,0);
+        if (Input.GetKey("left"))
+        {
+            x -= 50;
+        }
+        if (Input.GetKey("right"))
+        {
+            x += 50;
+        }
+        if (Input.GetKey("up"))
+        {
+            y += 50;
+        }
+        if (Input.GetKey("down"))
+        {
+            y -= 50;
+        }
+        rigidBody.velocity = new Vector3(x, y, 0);
+    }
+}
