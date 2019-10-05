d65a52e05fad406d3d9c870f05daf96a1c89e083
Tyraen1er <Emmanuel.Ferrandini@yahoo.fr>
2019-10-05 17:22:31 +0200

    ajout deplacement salle

--- Assets/Perso.cs
+++ Assets/Perso.cs
@@ -6,10 +6,14 @@ public class Perso : MonoBehaviour
 {
 
    	private Rigidbody2D rigidBody;
+    public float vitesse = 1;
+    public float rotation = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+     	rigidBody.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
     // Update is called once per frame
@@ -17,23 +21,38 @@ public class Perso : MonoBehaviour
     {
         float x = 0;
         float y = 0;
-        rigidBody.velocity = new Vector3(0,0,0);
         if (Input.GetKey("left"))
         {
-            x -= 50;
+            x -= 100;
         }
         if (Input.GetKey("right"))
         {
-            x += 50;
+            x += 100;
         }
         if (Input.GetKey("up"))
         {
-            y += 50;
+            y += 100;
         }
         if (Input.GetKey("down"))
         {
-            y -= 50;
+            y -= 100;
         }
-        rigidBody.velocity = new Vector3(x, y, 0);
+        rigidBody.velocity = new Vector2(x * vitesse, y * vitesse);
+        if (x == 100 && y == 100)
+            rigidBody.rotation = 225;
+        else if (x == -100 && y == 100)
+            rigidBody.rotation = -45;    
+        else if (x == 100 && y == -100)
+            rigidBody.rotation = 135;
+        else if (x == -100 && y == -100)
+           rigidBody.rotation = 45;
+        else if (x == 100)
+            rigidBody.rotation = 180;
+        else if (x == -100)
+            rigidBody.rotation = 0;
+        else if (y == -100)
+            rigidBody.rotation = 90;
+        else if (y == 100)
+            rigidBody.rotation = 270;
     }
 }
